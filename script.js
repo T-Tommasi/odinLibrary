a06321016abf1ef74f10d4514f3a51a61e26438f
T-Tommasi <ttommasi1945@gmail.com>
2024-12-27 15:28:04 +0100

    (MOD) Refactored book submitting logic
    - added basic modular logic for adding books
    - added logic for appending library content to the DOM
    - refactored logic for appending listeners to the buttons
    - initiated initializer function
    
    (RM) Removed obsolete code
    
    - removed HTML status tag and condition
    
    (FIX) Fixed wron call on some functions
    
    - fixed bookName being mispelled in a few functions
    - fixed naming convention for ease of use
    - fixed missing '.' on querySelector invocation

--- script.js
+++ script.js
@@ -8,8 +8,10 @@ function Library () {
             this._description = description;
         }
 
+        static {console.log(this)}
+
         get bookDetails() {
-            return this
+            return this //Easier to remember
         }
     }
 
@@ -18,26 +20,75 @@ function Library () {
         let author = document.querySelector('#author').value;
         let yearOfRelease = document.querySelector('#yearOfRelease').value;
         let description = document.querySelector('#description').value;
-        let book ={bookName, author, yearOfRelease, description};
+        let book = {bookName, author, yearOfRelease, description};
+        console.log(book);
         return book
     }
 
-    function Listeners() {
-        function dialogListener(target, source) {
-            source.addEventListener('click', () => {
-                target.showModal();
-            })
-        }
-        function closeListener(target, source) {
-            source.addEventListener('click', () => {
-                target.close()
-            })
-        }
+    function dialogListener(target, source) {
+        source.addEventListener('click', () => {
+            target.showModal();
+        })
+    }
+
+    function closeListener(target, source) {
+        source.addEventListener('click', () => {
+            target.close()
+        })
     }
 
     function createBook() {
-        let book = retrieveFormData();
-        newBook = new Book(book.bookName,book.author,book.yearOfRelease,book.description);
-        memorizedBooks.push(newBook);
+        let book = retrieveFormData(); //Retrieve the values from the HTML form and insert them as an object into a variable
+        newBook = new Book(book.bookName,book.author,book.yearOfRelease,book.description); //Create a newBook element
+        memorizedBooks.push(newBook); //Push it to memory
+        console.log('------');
+        console.log(newBook);
+        console.log('------');
+        return
+    }
+
+    function submitBook(button) {
+        console.log(`${button} has been pressed!`)
+        button.addEventListener('click', () => {
+            createBook();
+            console.log(`book pushed to library!`)
+            console.log(memorizedBooks)
+        })
+    }
+
+    function updateCards(array) {
+        const CARDCONTAINER = document.querySelector('.cardContainer')
+        for (let book of array) {
+            let bookCard = document.createElement('div');
+            bookCard.classList.add('card');
+            bookCard.classList.add(`${book._bookName}`)
+
+            let title = document.createElement('strong');
+            title.textContent = `${book._bookName}`;
+            let author = document.createElement('p');
+            author.textContent = `${book._author}`;
+            let yearOfRelease = document.createElement('p');
+            yearOfRelease.textContent = `${book._yearOfRelease}`;
+            let description  = document.createElement('p');
+            description.textContent  = `${book._description}`;
+
+            let content = [title,author,yearOfRelease,description]
+            for (let element of content) {
+                bookCard.appendChild(element)
+            }
+            if (!document.querySelector(`.${book._bookName}`)) {
+                CARDCONTAINER.appendChild(bookCard)
+            } else {
+                console.log(`the book titled: ${book._bookName} is already present in the DOM`)
+            }
+        }
+        console.log('-----')
+    }
+
+    function initializer() {
+        const NEWBOOKDIALOGBUTTON = document.querySelector('#submitBook');
+        const NEWBOOKDIALOG = document.querySelector('dialog'); //should probably update this so that i can have a system for removing previously stored books... but eeeh, thats beyond what is requested for now.
+        const SUBMITBOOKTOMEMORY = document.querySelector('#submitButton');
+
     }
 }
